804dbb4d3191bea08ed3e9fa2a38523bc0b741de
Becca Wyland <rwyland@uwalumni.com>
2021-07-22 20:06:17 -0500

    create nav component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
 import React from 'react';
+import Nav from './components/Nav'
 import About from './components/About';
 
 function App() {
---
+++ src/components/Nav/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Nav() {
+    return (
+        <header>
+            <h1>Nav</h1>
+        </header>
+    );
+}
+
+export default Nav;
