619f2363744e4e2fe77cdb4bd55798ae091e0955
Becca Wyland <rwyland@uwalumni.com>
2021-07-22 21:23:26 -0500

    import photo to portfolio

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
+// import photos here
+// import photo from "../../assets/small/portfolio/.png";
 
 function Portfolio(props) {
     const currentProject = {
@@ -8,8 +11,14 @@ function Portfolio(props) {
 
     return (
         <section>
-            <h2>{currentProject.name}</h2>
+            <h2>{capitalizeFirstLetter(currentProject.name)}</h2>
+
             <p>{currentProject.name}</p>
+
+            <div>
+                {/* notice use of img-thumbnail here, we may have to write the css? */}
+                <img src={photo} alt="Portoflio example" className="img-thumbnail mx-1" />
+            </div>
         </section>
     );    
 }
