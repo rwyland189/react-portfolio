2eac1a5f66edeb3081ca1eb2ae14868e23bf34ed
Becca Wyland <rwyland@uwalumni.com>
2021-07-22 19:58:39 -0500

    import about component into app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import About from './components/About';
 
 function App() {
   return (
     <div>
-      
+      <main>
+        <About></About>
+      </main>
     </div>
   );
 }
