936bb59f5516d4a899d7c104727e1a91b3310ec5
Becca Wyland <rwyland@uwalumni.com>
2021-07-23 20:34:49 -0500

    lift state to app.js and pass react hooks through nav

--- src/App.js
+++ src/App.js
@@ -4,9 +4,24 @@ import About from './components/About';
 import Portfolio from './components/Portfolio';
 
 function App() {
+  const [components] = useState([
+    { name: 'about me' },
+    { name: 'portfolio' },
+    { name: 'contact me'},
+    { name: 'resume' }
+  ]);
+
+  const [currentComponent, setCurrentComponent] = useState(components[0]);
+
+
+
   return (
     <div>
-      <Nav></Nav>
+      <Nav
+        components={components}
+        setCurrentComponent={setCurrentComponent}
+        currentComponent={currentComponent}
+      ></Nav>
       <main>
         <Portfolio></Portfolio>
         <About></About>
